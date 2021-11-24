64994e25fd0b9aa240f96a25f506db05c17eedde
Snider <snider@lt.hn>
2021-11-24 21:02:55 +0000

    documents the route guard that controls setup/login/authed

--- src/app/modules/auth/route.guard.ts
+++ src/app/modules/auth/route.guard.ts
@@ -13,6 +13,23 @@ export class AuthGuard implements CanActivate {
 	) {
 	}
 
+	/**
+	 * Handles logged out access
+	 * Run during routing to check if the user is logged in or not, we do this check if
+	 * getAuthStatus() returns true, the username variable is set, to logout simply clear the username
+	 *
+	 * If the user is not authenticated this function acts in one of two ways
+	 *
+	 * 1) if ~/Lethean/users is empty perform first run / welcome
+	 * 2) if users do exsist, take to the /login page
+	 *
+	 * if APP_CONFIG.production === false auth check is bypassed, but without login decryption cant happen
+	 * you may or may not have to login to develop, for now at least
+	 *
+	 * @param {ActivatedRouteSnapshot} route
+	 * @param {RouterStateSnapshot} state
+	 * @returns {boolean | Promise<boolean>}
+	 */
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
