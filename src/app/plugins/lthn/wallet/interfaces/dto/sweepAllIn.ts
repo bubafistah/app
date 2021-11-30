3d8e86f63af89860dcb5ae9d24a92fd696c06379
Snider <snider@lt.hn>
2021-11-30 10:10:39 +0000

    code documentation

--- src/app/plugins/lthn/wallet/interfaces/dto/sweepAllIn.ts
+++ src/app/plugins/lthn/wallet/interfaces/dto/sweepAllIn.ts
@@ -1,22 +1,42 @@
 /**
- * @param address Destination public address
- * @param priority (Optional)
- * @param mixin Number of outpouts from the blockchain to mix with (0 means no mixing).
- * @param unlock_time  unsigned int; Number of blocks before the monero can be spent (0 to not add a lock).
- * @param payment_id (Optional) Random 32-byte/64-character hex string to identify a transaction.
- * @param get_tx_keys (Optional) Return the transaction keys after sending.
- * @param below_amount (Optional)
- * @param do_not_relay (Optional)
- * @param get_tx_hex (Optional) return the transactions as hex encoded string.
+ * SweepAll request payload
+ *
+ * @property address string
+ * @property priority number
+ * @property mixin string
+ * @property unlock_time number
+ * @property payment_id string
+ * @property get_tx_keys boolean
+ * @property below_amount number
+ * @property do_not_relay boolean
+ * @property get_tx_hex boolean
  */
 export interface SweepAllIn {
+
+	/** Destination public address */
 	address: string;
+
+	/** (Optional) */
 	priority?: number;
+
+	/** Number of outpouts from the blockchain to mix with (0 means no mixing). */
 	mixin: number;
+
+	/** unsigned int; Number of blocks before the monero can be spent (0 to not add a lock). */
 	unlock_time: number;
+
+	/** (Optional) Random 32-byte/64-character hex string to identify a transaction. */
 	payment_id?: string;
+
+	/** (Optional) Return the transaction keys after sending. */
 	get_tx_keys?: boolean;
+
+	/** (Optional) */
 	below_amount?: number;
+
+	/** (Optional)  */
 	do_not_relay?: boolean;
-	get_tx_hex: boolean;
+
+	/** (Optional) return the transactions as hex encoded string. */
+	get_tx_hex?: boolean;
 }
