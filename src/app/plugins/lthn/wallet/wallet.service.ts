e7f02b003948ec289dac7bd14be062547726c734
Snider <snider@lt.hn>
2021-11-30 13:09:52 +0000

    get wallets list, display in sub menu

--- src/app/plugins/lthn/wallet/wallet.service.ts
+++ src/app/plugins/lthn/wallet/wallet.service.ts
@@ -18,13 +18,59 @@ export class WalletService {
     return this.rpc.startWallet()
   }
 
-  /**
-   * Restore wallet
-   *
-   * @param {RestoreWallet} req
-   * @returns {Promise<AxiosResponse<any>>}
-   */
-  restoreWallet(req: RestoreWallet){
-    return this.rpc.restoreWallet(req)
-  }
+	/**
+	 * Restore wallet
+	 *
+	 * @param {RestoreWallet} req
+	 * @returns {Promise<AxiosResponse<any>>}
+	 */
+	restoreWallet(req: RestoreWallet) {
+		return this.rpc.restoreWallet(req);
+	}
+
+	/**
+	 * Open a known wallet
+	 *
+	 * @param {OpenWallet} req
+	 * @returns {Promise<AxiosResponse<any>>}
+	 */
+	openWallet(req: OpenWallet) {
+		return this.rpc.openWallet(req);
+	}
+
+	/**
+	 * Get wallet balance
+	 *
+	 * @param {Balance} req
+	 * @returns {Observable<Balance>}
+	 */
+	async getBalance(): Promise<Balance> {
+		return await this.rpc.getBalance()
+	}
+	/**
+	 * Gets the list of known wallets from the file system
+	 */
+	getWalletList() {
+		this.fs.listFiles('/wallets').then((data) => {
+			if (data.length > 0) {
+				for (let dat of data) {
+					if (!dat.endsWith('.keys') && !dat.endsWith('.txt')) {
+						this.wallets.push(dat);
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * Returns the wallets we know about
+	 *
+	 * @returns {string[]}
+	 */
+	walletList() {
+		if (this.wallets.length === 0) {
+			this.getWalletList();
+		}
+		return this.wallets;
+	}
 }
