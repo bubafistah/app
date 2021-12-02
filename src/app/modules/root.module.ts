7473a0c0e6299b8d954619a484a9e99e62b4ff6c
Snider <snider@lt.hn>
2021-12-02 14:19:23 +0000

    adds ssr, helps catch runtime errors

--- src/app/modules/root.module.ts
+++ src/app/modules/root.module.ts
@@ -1,17 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
 import {RootComponent} from './root.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import {AuthGuard} from './auth/route.guard';
 import {FlexModule} from '@angular/flex-layout';
 import {ConsoleModule} from '@plugin/console/console.module';
 import {MatTabsModule} from '@angular/material/tabs';
 import {WalletModule} from '@plugin/lthn/wallet/wallet.module';
 import {TranslateModule} from '@ngx-translate/core';
 import {MatDividerModule} from '@angular/material/divider';
-import {ModalComponent} from '@service/ui/modal/modal.component';
 
 @NgModule({
 	declarations: [RootComponent],
@@ -22,11 +19,11 @@ import {ModalComponent} from '@service/ui/modal/modal.component';
 		MatCardModule,
 		MatButtonModule,
 		FlexModule,
-		ConsoleModule,
 		MatTabsModule,
 		WalletModule,
 		TranslateModule,
-		MatDividerModule
+		MatDividerModule,
+		ConsoleModule
 	]
 })
 export class RootModule {
