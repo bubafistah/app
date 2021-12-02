1d69d7657edb44c580a1adf1507ce3cd6c578c5d
Snider <snider@lt.hn>
2021-12-02 16:13:24 +0000

    console restore

--- src/app/plugins/console/console.component.ts
+++ src/app/plugins/console/console.component.ts
@@ -1,10 +1,7 @@
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {NgTerminal} from 'ng-terminal';
 import {WebsocketService} from '@service/websocket.service';
-import {Component, OnInit, ViewChild, AfterViewInit, ViewEncapsulation, ChangeDetectionStrategy, Input, ChangeDetectorRef, PLATFORM_ID, Inject} from '@angular/core';
-import { Subject } from 'rxjs';
-import { FormControl } from '@angular/forms';
-import {DisplayOption, NgTerminal, FunctionsUsingCSI} from 'ng-terminal';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import {isPlatformBrowser} from '@angular/common';
+
 
 @Component({
 	selector: 'lthn-app-console',
@@ -16,25 +13,11 @@ import {isPlatformBrowser} from '@angular/common';
 
 })
 export class ConsoleComponent implements OnInit, AfterViewInit {
+	@ViewChild('term', { static: true }) child: NgTerminal;
 
 	@Input() attach  = 'lethean-wallet-rpc';
 	private command: string[] = []
 	private sub$;
-	title = 'NgTerminal Live Example';
-	color = 'accent';
-
-	public resizable: boolean;
-	public fixed = false;
-
-	disabled = true;
-	rowsControl = new FormControl();
-	colsControl = new FormControl();
-	inputControl = new FormControl();
-
-	displayOption: DisplayOption = {};
-	displayOptionBounded: DisplayOption = {};//now it's not used
-	@Inject(PLATFORM_ID) platformId: Object
-	@ViewChild('term', {static: false}) child: NgTerminal;
 	constructor(private ws: WebsocketService, private ref: ChangeDetectorRef) {
 		this.ref.detach()
 		setInterval(() => {
@@ -44,23 +27,17 @@ export class ConsoleComponent implements OnInit, AfterViewInit {
 	}
 
 	ngOnInit(): void {
-		if(isPlatformBrowser(this.platformId)) {
-			let that = this;
-			this.disabled = false
-			this.rowsControl.setValue(10);
-			this.colsControl.setValue(40);
-			this.ref.detectChanges();
-			this.ws.connect().subscribe((data) => {
-				if (this.attach === data[0]) {
-					this.child.write(data[1] + '\r\n');
-					that.ref.markForCheck()
-				}
+		let that = this;
+		this.ref.detectChanges();
+		this.ws.connect().subscribe((data) => {
+			if(this.attach === data[0]) {
+				this.child.write(data[1] + '\r\n');
+				that.ref.markForCheck()
+			}
 
-			})
+		})
+
 			this.changeStream(this.attach)
-		}else{
-			this.disabled = true
-		}
     }
 
 	getSub(){
@@ -72,114 +49,37 @@ export class ConsoleComponent implements OnInit, AfterViewInit {
 		this.ref.markForCheck()
 	}
 
-//	ngAfterViewInit() {
-//		const that = this;
-//		this.child.write('$ ');
-//		if(this.child.keyEventInput) {
-//
-//			this.child.keyEventInput.subscribe((e) => {
-//				//console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
-//
-//				const ev = e.domEvent;
-//				const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
-//
-//				if (ev.keyCode === 13) {
-//
-//					//console.log(`cmd:letheand:${this.command.join('')}`)
-//					that.ws.sendMessage(`cmd:letheand:${this.command.join('')}\n`)
-//					this.command = []
-//					//this.child.write('\r\n$ ');
-//					this.ref.detectChanges();
-//				} else if (ev.keyCode === 8) {
-//					 this.command.pop()
-//					if (this.child.underlying.buffer.active.cursorX > 0) {
-//						this.child.write('\b \b');
-//						this.ref.detectChanges();
-//					}
-//				} else if (printable) {
-//					this.command.push(e.key);
-//					this.child.write(e.key);
-//					this.ref.detectChanges();
-//				}
-//				//console.log(this.command.join(""))
-//			});
-//		}
-//
-//	}
 	ngAfterViewInit() {
-		if(isPlatformBrowser(this.platformId)) {
-			this.invalidate();
-			this.child.write('$ ');
-			this.child.keyInput.subscribe((input) => {
-				//do nothing because it will be replaced keyEventInput
-			})
+		const that = this;
+		this.child.write('$ ');
+		if(this.child.keyEventInput) {
 
-			this.child.keyEventInput.subscribe(e => {
-				console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
+			this.child.keyEventInput.subscribe((e) => {
+				//console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
 
 				const ev = e.domEvent;
 				const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
 
 				if (ev.keyCode === 13) {
-					this.child.write('\n' + FunctionsUsingCSI.cursorColumn(1) + '$ '); // \r\n
+
+					//console.log(`cmd:letheand:${this.command.join('')}`)
+					that.ws.sendMessage(`cmd:letheand:${this.command.join('')}\n`)
+					this.command = []
+					this.child.write('\r\n$ ');
+					this.ref.detectChanges();
 				} else if (ev.keyCode === 8) {
-					// Do not delete the prompt
-					if (this.child.underlying.buffer.active.cursorX > 2) {
+					 this.command.pop()
+					if (this.child.underlying.buffer.active.cursorX > 0) {
 						this.child.write('\b \b');
+						this.ref.detectChanges();
 					}
 				} else if (printable) {
+					this.command.push(e.key);
 					this.child.write(e.key);
+					this.ref.detectChanges();
 				}
-			})
-			this.rowsControl.valueChanges.subscribe(() => {
-				this.invalidate()
-			});
-			this.colsControl.valueChanges.subscribe(() => {
-				this.invalidate()
+				//console.log(this.command.join(""))
 			});
 		}
-	}
-	writeSubject = new Subject<string>();
-	write() {
-		this.writeSubject.next(eval(`'${this.inputControl.value}'`));
-	}
-
-	keyInput: string;
-	onKeyInput(event: string) {
-		this.keyInput = event;
-	}
-
-	get displayOptionForLiveUpdate() {
-		return JSON.parse(JSON.stringify(this.displayOption));
-	}
-
-	invalidate() {
-		if (this.resizable)
-			this.displayOption.activateDraggableOnEdge = { minWidth: 100, minHeight: 100 };
-		else
-			this.displayOption.activateDraggableOnEdge = undefined;
-		if (this.fixed)
-			this.displayOption.fixedGrid = { rows: this.rowsControl.value, cols: this.colsControl.value };
-		else
-			this.displayOption.fixedGrid = undefined;
-		this.child.setDisplayOption(this.displayOption);
-	}
-
-	resizableChange(event: MatSlideToggleChange) {
-		this.resizable = event.checked;
-		if (this.resizable){
-			this.child.setStyle({"border": "4px solid #85858a"});
-			this.fixed = false;
-		}
-		this.invalidate();
-	}
-
-	fixedChange(event: MatSlideToggleChange) {
-		this.fixed = event.checked;
-		if (this.fixed){
-			this.child.setStyle({"border": "unset"});
-			this.resizable = false;
-		}
-		this.invalidate();
 	}
 }
